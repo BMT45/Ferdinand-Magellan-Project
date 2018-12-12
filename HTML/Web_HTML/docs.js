514f9ac93227c84c79d3d3dbc3f4fab6dd66780d
Rachel Garstecki <42811963+RLG95@users.noreply.github.com>
2018-12-11 21:15:09 -0500

    Add files via upload

---
+++ HTML/Web_HTML/docs.js
@@ -0,0 +1,64 @@
+/* Rachel */
+
+function init() {
+    var fieldset = document.getElementsByTagName('input');
+    for (var i = 0; i < fieldset.length; i++) {
+        fieldset[i].addEventListener('click', toggle, false);
+    }
+}
+
+function toggle() {
+    var id = this.id;
+    switch (id) {
+        case "perstoggle": {
+            var pers = document.getElementsByClassName("persName");
+            for (var i = 0; i < pers.length; i++) {
+                pers[i].style.backgroundColor = "#E0C5FE"
+            }
+        };
+        break;
+        case "placetoggle": {
+            var place = document.getElementsByClassName("placeName");
+            for (var i = 0; i < Writ.length; i++) {
+                place[i].style.backgroundColor = "#C5E9FE"
+            }
+        };
+        break;
+        case "datetoggle": {
+            var date = document.getElementsByClassName("date");
+            for (var i = 0; i < date.length; i++) {
+                date[i].style.backgroundColor = "#AAFEB3"
+            }
+    };
+     break;
+        case "egtoggle": {
+            var eg = document.getElementsByClassName("ethnicGroup");
+            for (var i = 0; i < loc.length; i++) {
+                eg[i].style.backgroundColor = "#FEE2AA"
+            }
+    };
+     break;
+        case "ranktoggle": {
+            var rank = document.getElementsByClassName("rank");
+            for (var i = 0; i < org.length; i++) {
+                rank[i].style.backgroundColor = "#A1B6EF"
+            }
+    };
+     break;
+        case "termtoggle": {
+            var term = document.getElementsByClassName("term");
+            for (var i = 0; i < City.length; i++) {
+                term[i].style.backgroundColor = "#CDCCCC"
+            }
+        };
+         break;
+        case "nametoggle": {
+            var name = document.getElementsByClassName("name");
+            for (var i = 0; i < Nat.length; i++) {
+               name[i].style.backgroundColor = "#A1B6EF"
+            }
+    };
+    }
+    }
+
+window.onload = init;
